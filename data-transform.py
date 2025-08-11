2437dffbfd97ac217b8cf32ded2f7b905bd6ab34
Nadine Siccion <nv.siccion@gmail.com>
2025-08-12 01:00:12 +0800

    Implement get recent csv code
    Implement get recent csv code. Add logging
    functionality later.

--- data-transform.py
+++ data-transform.py
@@ -1,9 +1,36 @@
-import pandas
+#%%
+import pandas as pd
 import os
+from pathlib import Path
 
+#%%
 # read latest CSV from the folder csv
-csv_files = os.listdir("./csv")
+BASE_DIR = Path.cwd()
+CSV_DIR = BASE_DIR / "csv"
+csv_files = os.listdir(CSV_DIR)
+
+if ".gitignore" in csv_files:
+    csv_files.remove(".gitignore")
+csv_files.sort()
 for file in csv_files:
-    pass
+    print(file)
+
+latest_csv = csv_files[-1]
+print(f'Latest CSV is {latest_csv}')
 
 # create df
+df = pd.read_csv(CSV_DIR/latest_csv)
+print(df.head(10))
+
+# CLEANING
+
+
+
+
+
+
+
+# %%
+# DEBUGGING CODE
+print(os.getcwd())
+# %%
