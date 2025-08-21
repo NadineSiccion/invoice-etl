52c5450e693c2d0b2316da412756adde27822d2b
Nadine Siccion <nv.siccion@gmail.com>
2025-08-21 19:34:50 +0800

    Implement modern logging
    Logs will be logged out as json in the logs
    folder. Some files have been added for its
    configuration.

--- data-transform.py
+++ data-transform.py
@@ -4,6 +4,35 @@ import os
 from pathlib import Path
 from datetime import datetime
 
+# Code for Logging
+import atexit
+import json
+import logging.config
+import logging.handlers
+import pathlib
+import mylogger
+
+logger = logging.getLogger(__name__)  # __name__ is a common choice
+
+def setup_logging():
+    config_file = pathlib.Path("2-stderr-json-file.json")
+    with open(config_file) as f_in:
+        config = json.load(f_in)
+
+    logging.config.dictConfig(config)
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
+
+setup_logging()
+logging.basicConfig(level="INFO")
+
+logger.debug("debug message", extra={"x": "hello"})
+logger.info("info message")
+logger.warning("warning message")
+logger.error("error message")
+logger.critical("critical message")
 
 #%% Paths
 BASE_DIR = Path.cwd()
