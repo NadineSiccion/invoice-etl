1f62a50dc9477caee6cc891230b9943a5eadae4b
Nadine Siccion <nv.siccion@gmail.com>
2025-09-17 23:59:53 +0800

    Complete make_dim_file and make_dim_category
    Some code cleaning should be the next step,
    then loading to Google BigQuery.

--- data-transform.py
+++ data-transform.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 from pathlib import Path
 from datetime import datetime
+import re
 
 # Code for Logging
 import atexit
@@ -36,6 +37,7 @@ def setup_logging():
     if queue_handler is not None:
         queue_handler.listener.start()
         atexit.register(queue_handler.listener.stop)
+
 setup_logging()
 
 # Initialize logging
@@ -56,13 +58,16 @@ CSV_DIR = BASE_DIR / "csv"
 CATEGORY_PATH = BASE_DIR / "categories.json"
 
 # Create DataFrame out of the latest CSV
-csv_files = os.listdir(CSV_DIR)
-if ".gitignore" in csv_files:
-    csv_files.remove(".gitignore")
-csv_files.sort()
-for file in csv_files:
-    print(file)
-latest_csv = csv_files[-1]
+def get_latest_csv_path(csv_dir) -> str:
+    csv_files = os.listdir(csv_dir)
+    if ".gitignore" in csv_files:
+        csv_files.remove(".gitignore")
+    csv_files.sort()
+    for file in csv_files:
+        print(file)
+    return csv_files[-1]
+
+latest_csv = get_latest_csv_path(CSV_DIR)
 print(f'Latest CSV is {latest_csv}')
 logger.info('Latest CSV is {latest_csv}')
 
@@ -73,6 +78,12 @@ logger.info("Initial DataFrame created.")
 
 # %% 
 
+def change_month_to_number(name):
+    for substr, num in month_substrings.items():
+        if substr in name:
+            name = name.replace(substr, num, 1)  # Replace only the first occurrence
+    return name
+
 # CLEANING
 # Set up DESCRIPTION2 column
 desc_2 = ['N/A']
@@ -82,6 +93,68 @@ print(df.head())
 print(df.tail())
 
 # TODO: Parse start and end dates, put it in as a column.
+#%%
+
+def extract_date(arr:list) -> datetime.date:
+    out = []
+
+    # Day
+    pattern = r"\D+"  # \D matches any non-digit character
+    match = re.search(pattern, arr[0])
+    if match:
+        print(match.group())
+        out = [arr[0].replace(match.group(0), "").zfill(2)]
+    else:
+        raise Exception("Issue in day input for extract_date.") 
+    
+    # Month
+    for month_sub in list(month_substrings.keys()):
+        if month_sub in arr[1].lower():
+            out.append(str(month_substrings[month_sub]).zfill(2))
+    
+    # Year
+    out.append(arr[2])
+    
+    # Convert to datetime
+    output = datetime(int(out[2]), int(out[1]), int(out[0])).date()
+    # print('Extracted date:', output)
+    return output
+
+#%%
+def make_dim_file(df:pd.DataFrame) -> pd.DataFrame:
+    coverage_dict = {'file_key': [], 
+                     'file_name':[], 
+                     'start_dates': [], 
+                     'end_dates': []}
+
+    for desc, src_name in zip(df['DESCRIPTION'], df['Source.Name']):
+        print('desc:' + desc)
+        if 'Coverage Period' in desc:
+            temp = desc.split(' ')
+            start_date = temp[2:5]
+            end_date = temp[6:9]
+            coverage_dict['start_dates'].append(extract_date(start_date))        
+            coverage_dict['end_dates'].append(extract_date(end_date))
+        else:
+            coverage_dict['start_dates'].append(None)        
+            coverage_dict['end_dates'].append(None)
+        coverage_dict['file_key'].append(change_month_to_number(src_name.lower()))
+        coverage_dict['file_name'].append(src_name)
+    
+    print([
+        len(coverage_dict['file_key']),
+        len(coverage_dict['file_name']),
+        len(coverage_dict['start_dates']),
+        len(coverage_dict['end_dates'])
+        ])
+    print(coverage_dict['file_key'])
+        
+    coverage_df = pd.DataFrame(coverage_dict)
+    return coverage_df
+
+dim_coverages = make_dim_file(df)
+dim_coverages.head()
+#%%
 
 # Set up Category column
 def get_category(desc1, desc2):
@@ -109,9 +182,6 @@ def get_new_filename(source_name, month_map):
     return new_name
 
 
-# Make the File Dimension
-def get_coverage_period(description):
-    pass
 
 
 # Make the transaction keys
@@ -120,11 +190,6 @@ def get_coverage_period(description):
 # Make the category
 
 # %% Set up Primary key column / Transaction ID
-def change_month_to_number(name):
-    for substr, num in month_substrings.items():
-        if substr in name:
-            name = name.replace(substr, num, 1)  # Replace only the first occurrence
-    return name
 
 #%%
 def get_primary_key(source_name:str, category:str):
@@ -137,6 +202,7 @@ df['PRIMARY'] = primary_s
 print(df.head())
 print(df.tail())
 logger.info("Primary keys made")
+
 
 # Clean data and export csv to "out" folder
 df = df[df['UNIT PRICE'].notna() & df['CATEGORY'].notnull()]
@@ -178,5 +244,10 @@ print(dim_category)
 logging.info("dim_category df made")
     
 # %%
-file_key_s = pd.Series([change_month_to_number(item.lower()) for item in df['file_name']])
 # TODO: def make_dim_file
+
+
+#%% debug cell
+test = [1,2,3,4,5,6]
+print(test[0:2])
+# %%
